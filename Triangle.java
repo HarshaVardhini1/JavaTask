f2b68e9dcd52182397a2fc4dfea9de7eea3bf4e2
HarshaVardhini1 <61868588+HarshaVardhini1@users.noreply.github.com>
2020-03-06 15:33:33 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle{
+	public static void main(String ar[]){
+		int num=1;
+		for(int i=4;i>=1;i--){
+			for(int j=i;j>0;j--){			
+				System.out.print(num+" ");
+				num++;
+			}
+			System.out.println();
+		}
+	}
+}	

6c9d7fb10a9e2c90b2267712cae764405eb229c1
HarshaVardhini1 <61868588+HarshaVardhini1@users.noreply.github.com>
2020-03-09 18:49:05 +0530

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -2,7 +2,7 @@ public class Triangle{
 	public static void main(String ar[]){
 		int num=1;
 		for(int i=4;i>=1;i--){
-			for(int j=i;j>0;j--){			
+			for(int j=0;j<i;j++){			
 				System.out.print(num+" ");
 				num++;
 			}

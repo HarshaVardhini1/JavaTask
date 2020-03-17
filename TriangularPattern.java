1c0d8d02bd9c0a6a7994479d58dc0a7dfff0b3c0
HarshaVardhini1 <61868588+HarshaVardhini1@users.noreply.github.com>
2020-03-17 12:59:19 +0530

    Update TriangularPattern.java

--- TriangularPattern.java
+++ TriangularPattern.java
@@ -3,14 +3,13 @@ public class TriangularPattern
 	public static void main(String ar[])
 	{
 		int num=7;
-		int j;
 		for(int i=1;i<=num;i++)
 		{
-			for(j=1;j<i;j++)
+			for(int j=1;j<i;j++)
 			{
 				System.out.print(j+" ");
 			}
-			for(int k=j;k>=1;k--)
+			for(int k=i;k>=1;k--)
 			{
 				System.out.print(k+" ");
 			}

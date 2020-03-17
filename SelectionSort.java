c7ebd21cc54a19d1282d16e542d275fddc6986f8
HarshaVardhini1 <61868588+HarshaVardhini1@users.noreply.github.com>
2020-03-17 14:35:03 +0530

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,23 +1,33 @@
 import java.util.Arrays;
+import java.util.Scanner;
+
 public class SelectionSort
 {
 	public static void main(String ar[])
 	{
-		int arr[]={18,9,33,4,84,32};
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the total number of elements in array : ");
+		int length=input.nextInt();
+		int arr[]=new int[length];
+		System.out.println("Enter the numbers : ");	
+		for(int i=0;i<length;i++)
+		{
+			arr[i]=input.nextInt();
+		}
+		//int arr[]={18,9,33,4,84,32};
 		
 		for(int i=0;i<arr.length-1;i++)
 		{
-			int index=i;
 			for(int j=i+1;j<arr.length;j++)
 			{
-				if(arr[j]<arr[index])
+				if(arr[j]<arr[i])
 				{
-					index=j;
+					int temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
 				}
 			}
-			int temp=arr[index];
-			arr[index]=arr[i];
-			arr[i]=temp;
+			
 		}
 		
 		System.out.print(Arrays.toString(arr));

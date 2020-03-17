8f681b751becf1401721429b672bfac8c04086d0
HarshaVardhini1 <61868588+HarshaVardhini1@users.noreply.github.com>
2020-03-17 14:55:47 +0530

    Create RemoveString.java

---
+++ RemoveString.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+public class RemoveString
+{
+	public static void main(String ar[])
+	{
+		String sentance="Hello This is Hello";
+		String remove="He";
+		String result="";
+		String resultArr[];
+
+    if(sentance.contains(remove))
+		{
+			resultArr=sentance.split(remove);
+			for(int i=0;i<resultArr.length;i++)
+			{
+				result+=resultArr[i];
+			}
+			System.out.println(result);
+		}
+	}
+}
+
+/*String result=sentance.replace("e","");
+		System.out.println(result);*/
